8c4604f21ac81fc2d559e7ca6cb1a2215c1988e0
Milan2496 <mg101@kent.ac.uk>
2026-03-08 11:19:50 +0000

    Start Menu

---
+++ F1_Simulator/Retry_main.py
@@ -0,0 +1,15 @@
+
+
+def display_menu():
+    print("\n-----MENU-----")
+    print("1. View Grid")
+    print("2. Register Driver")
+    print("4. Start Qualifying")
+    print("5. Start Race")
+    opt = int(input("\nSelect option: "))
+    return opt
+
+def main():
+    display_menu()
+
+main()
