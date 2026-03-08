1d80b27330ad42aa1a53d638ce68fa831090eb37
Milan2496 <mg101@kent.ac.uk>
2026-03-08 11:25:04 +0000

    display menu

--- F1_Simulator/Retry_main.py
+++ F1_Simulator/Retry_main.py
@@ -4,12 +4,26 @@ def display_menu():
     print("\n-----MENU-----")
     print("1. View Grid")
     print("2. Register Driver")
-    print("4. Start Qualifying")
-    print("5. Start Race")
+    print("3. Start Qualifying")
+    print("4. Start Race")
+    print("5. Exit")
     opt = int(input("\nSelect option: "))
     return opt
 
 def main():
     display_menu()
 
+    while True:    
+        opt = display_menu()
+        if opt == 1:
+            print("View Grid")
+        if opt == 2:
+            print("Register Driver")
+        if opt == 3:
+            print("Start Qualifying")
+        if opt == 4:
+            print("Start Race")                 
+        elif opt == 5:
+             print("Exit")
+             return
 main()
