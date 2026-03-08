442068109b6fe02301a3f0d653d882cceaf0597c
Milan2496 <mg101@kent.ac.uk>
2026-03-08 10:56:48 +0000

    check that team only has 2 drivers when adding new driver, if team already has 2 drivers, program will inform user.

--- F1_Simulator/main.py
+++ F1_Simulator/main.py
@@ -1,3 +1,27 @@
 from models import name
 
-print(name)
+def display_menu():
+    print("\n--- F1 ---")
+    print("1. View Grid")
+    print("2. Register Driver")
+    print("3. Choose Strategy")
+    print("4. Start Qualifying")
+    print("5. Start Race")
+    opt = int(input("\nSelect option: "))
+    return opt
+
+
+
+def main():
+    
+    run = True
+    while True:    
+        opt = display_menu()
+        if opt == 1:
+            register_driver(name, number, team, skill)         
+        elif opt == 9:
+             print("Shutting down.")
+             return
+
+
+main()
