fead8975a5204c5d1b1ddcbdbc41314029a132bc
Milan2496 <mg101@kent.ac.uk>
2026-03-09 17:37:13 +0000

    adding changes to print lines

--- Retry_main.py
+++ Retry_main.py
@@ -3,13 +3,13 @@ import re
 
 
 def display_menu():
-    print("\n-----MENU-----")
+    print("\n--------MENU--------")
     print("1. View Grid")
     print("2. Reset Grid")
-    print("2. Register Driver")
-    print("3. Start Qualifying")
-    print("4. Start Race")
-    print("5. Exit")
+    print("3. Register Driver")
+    print("4. Start Qualifying")
+    print("5. Start Race")
+    print("6. Exit")
     opt = int(input("\nSelect option: "))
     return opt
 
@@ -19,22 +19,25 @@ def view_grid():
         csv_reader = csv.reader(csv_file)
 
         for line in csv_reader:
-            print(line)
+            new_name, new_number, new_team = line
+            print(f"Name: {new_name} |  #{new_number} | Team: {new_team}")
+            
 
 
 def reset_grid():
     while True:
-        decide = input("Are you sure you want to reset the grid? Select yes or no: ")
-        if decide == "YES":
+        decide = input("Are you sure you want to reset the grid? Select Yes or No: ")
+        decide = decide.title()
+        if decide == "Yes":
             with open ("drivers.csv", "w", newline='') as file:
                 writer = csv.writer(file)
                 writer.writerow(["Verstappen", "3", "Red Bull"])
                 writer.writerow(["Hadjar", "6", "Red Bull"])
                 writer.writerow(["Hamilton", "44", "Ferrari"])
                 writer.writerow(["Leclerc", "16", "Ferrari"])
-            print("Grid is reset")
+            print("GRID IS RESET")
             break
-        elif decide == "NO":
+        elif decide == "No":
             print("Routing back to menu . . .")
             break
         else:
@@ -55,7 +58,7 @@ def register_driver():
         try:
             new_number = int(input("Driver number (2-99): "))
             while new_number < 2 or new_number > 99:
-                new_number = int(input("Invalid number. Choose driver number (2-99): "))
+                new_number = int(input("Number Unavailable. Choose driver number (2-99): "))
         except ValueError:
             print("Invalid input")
         else:
@@ -66,19 +69,20 @@ def register_driver():
     
     while True:
         try:
-            new_team = int(input("Choose team (1.Audi, 2.Mercedes, 3.Cadillac): "))
+            new_team = int(input("Choose team - | 1.Mercedes | 2.Audi | 3.Mclaren |: "))
             while new_team not in team_list:
-                new_team = int(input("Invalid number. Try again: "))
+                new_team = int(input("Invalid Team. Choose team - | 1.Mercedes | 2.Audi | 3.Mclaren |: "))
             break
             
         except ValueError:
             print("Invalid input")
 
     if new_team == 1:
-        new_team = "Merc"
+        new_team = "Mercedes"
     elif new_team == 2:
-        new_team = "Red Bull"
-          
+        new_team = "Audi"
+    elif new_team == 3:
+        new_team = "Mclaren"      
 
     with open ("drivers.csv", "a", newline='') as file:
         writer = csv.writer(file)
@@ -90,13 +94,13 @@ def main():
     while True:    
         opt = display_menu()
         if opt == 1:
-            print("View Grid")
+            print("---------------VIEW GRID---------------")
             view_grid()
         if opt == 2:           
-            print("Reset Grid")
+            print("RESET GRID")
             reset_grid()
         if opt == 3:           
-            print("Register Driver")
+            print("-----REGISTER DRIVER-----")
             register_driver()
         if opt == 4:
             print("Start Qualifying")
