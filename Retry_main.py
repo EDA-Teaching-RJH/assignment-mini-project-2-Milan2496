f06dff3a861bd8aba588e0987c1dda12c954bf11
Milan2496 <mg101@kent.ac.uk>
2026-03-10 22:59:47 +0000

    finished the table and also found an easier wa yto limit number to 3 decimal points using .3f

--- Retry_main.py
+++ Retry_main.py
@@ -143,19 +143,24 @@ def qualifying():
     elif weather == "wet":
         time = 1.999
 
+    Pos = "Position"
+    Name = "Name"
+    Time = "Time"
+    print(f"{Pos:<9} {Name:<12} Time ")
+
     for line in driver:
         new_name, new_number, new_team = line
         lap_add = random.uniform(0, time)
         lap_time = round(lap_time + lap_add, 3)
         
         if re.search(r"^[0-9]{2}.[0-9]{2}+$", str(lap_time)):
-            print(f"{position:<10} - {new_name:<10} - 1:{lap_time:<10}0")
+            print(f"{position:<9} {new_name:<12} 1:{lap_time:.3f}0")
             position = position + 1
-        elif re.search(r"^[0-9]{2}.[0-9]{1}+$", str(lap_time)):
-            print(f"{position:<10} - {new_name:<10} - 1:{lap_time:<10}00")
+        elif re.search(r"^[0-9]{8}.[0-9]{1}+$", str(lap_time)):
+            print(f"{position:<9} {new_name:<12} 1:{lap_time:.3f}00")
             position = position + 1
         else:
-            print(f"{position:<10} - {new_name:<10} - 1:{lap_time}")
+            print(f"{position:<9} {new_name:<12} 1:{lap_time:.3f}")
             position = position + 1
 
 
